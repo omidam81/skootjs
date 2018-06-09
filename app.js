f246ddd3b8436f3aa14e4ec07c4f666f2d03e2f7
Omid Amiri <omid@Omids-MacBook-Pro.local>
2018-06-09 19:11:52 +0300

    changes

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//app.use('/', jobsRouter);
+app.use('/', jobsRouter);
 
 
 // catch 404 and forward to error handler
