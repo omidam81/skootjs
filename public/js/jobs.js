31a791348217f0560dffe2d3155a57a024e70881
Omid Amiri <omid@Omids-MacBook-Pro.local>
2018-07-09 03:15:34 +0300

    changes

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 $('.btn-view-job').click(function(){
     event.stopPropagation();
+    var id = $(this).data('job-id');
+    var title = $(this).data('job-title');
 
+    var href = `/job/${id}/${slugify(title)}`;
+    $('iframe').attr('src', href);
     $('iframe').show(100);
 })
 $('body').click(function(){
@@ -11,3 +15,13 @@ $('iframe').click(function(event ){
     event.stopPropagation();
 })
 });
+
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
--- routes/job.js
+++ routes/job.js
@@ -7,18 +7,44 @@ const Job = require('../models/job');
 
 const Business = require('../models/business');
 
-function parseParams(q) {
-    return { status: 'live' };
-}
-/* GET home page. */
-router.get('/jobs/:q*?', function(req, res, next) {
+function makeQuery(q) {
+    var x = { status: 'live' };
+    if (q.keyword && q.keyword > 0) {
+        var keys = q.keyword.split(" ");
+        var re = new RegExp(options.keyword, "i");
+        if (q.keyword.indexOf(' ') != -1) {
+            search = "";
+            var words = q.keyword.split(" ");
+            for (var i = 0; i < words.length; i++) {
+                search += "(?=.*" + words[i] + ")";
+            }
 
-    var query = parseParams(req.params.q);
-    
+            re = new RegExp(search, "i");
+            var or = [];
+            x.push({
+                keywords: {
+                    regexp: re
+                }
+            });
+        }
+    }
+    return x;
+}
+
+function getParams() {
+    return [];
+}
+
+function doSearch(req, res, params) {
+    if (!params || params.length == 0) {
+        params = getParams(req);
+    }
+    var query = makeQuery(params);
     try {
 
         Promise.all([
-            Job.find(query).select({ businessId: 1, title: 1, description: 1, typeOfSalary: 1, salaryFrom: 1, salaryTo: 1, location: 1, adddress: 1, informalAddress: 1 }).limit(req.query.limit)
+            Job.find(query).
+            select({ businessId: 1, title: 1, description: 1, typeOfSalary: 1, salaryFrom: 1, salaryTo: 1, location: 1, adddress: 1, informalAddress: 1 }).limit(req.query.limit)
             .populate({
                 path: 'businessId',
                 populate: { // 2nd level subdoc (get users in comments)
@@ -32,7 +58,7 @@ router.get('/jobs/:q*?', function(req, res, next) {
             const itemCount = result[1];
             const jobs = result[0];
             pagination = {
-                current : parseInt(req.query.page)
+                current: parseInt(req.query.page)
             };
 
             const pageCount = Math.ceil(itemCount / req.query.limit);
@@ -42,29 +68,56 @@ router.get('/jobs/:q*?', function(req, res, next) {
                 itemCount,
                 pagination,
                 pages: paginate.getArrayPages(req)(5, pageCount, req.query.page),
-                styles:[{src:'jobs.css'}],
-                scripts:[{src:'jobs.js'}]
+                styles: [{ src: 'jobs.css' }],
+                scripts: [{ src: 'jobs.js' }]
             });
         });
     } catch (err) {
         next(err);
     }
 
-
+}
+/* GET home page. */
+router.get('/jobs/:q*?', function(req, res, next) {
+    doSearch(req, res);
 });
+
+
 
 // router.get('/job/:id?*/:title', function(req, res, next) {
 //     res.render('jobs', { title: "job view pages" });
 // });
 
+function showJob(req, res) {
+    Job.findOne({ _id: req.params.id }).populate({
+        path: 'businessId',
+        populate: { // 2nd level subdoc (get users in comments)
+            path: "companyId",
+        }
+    }).then(function(job) {
+        res.render('job', {
+            title: "job view pages",
+            job: job,
+            styles: [{ src: 'jobs.css' }]
+        });
+    });
+}
+router.get('/job/:id/:title', function(req, res, next) {
+    showJob(req, res);
+})
 router.get('/job', function(req, res, next) {
-    res.render('job', { title: "job view pages", styles:[{src:'jobs.css'}] });
+    res.render('job', { title: "job view pages", styles: [{ src: 'jobs.css' }] });
 });
 
 router.get('/culture', function(req, res, next) {
-    res.render('culture', { title: "culture", styles:[{src:'jobs.css'}] });
+    res.render('culture', { title: "culture", styles: [{ src: 'jobs.css' }] });
 
 });
+
+router.get('/search', function(req, res, next) {
+    var params = [req.params.location, req.params.keyword];
+    doSearch(req, res, params);
+})
 
 
 
